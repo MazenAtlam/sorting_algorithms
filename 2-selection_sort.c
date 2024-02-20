59b156ed3750e3958e485bc5e28b807f50f2a842
MazenAtlam <mazenatef5510@gmail.com>
2024-02-20 14:38:15 -0800

    selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,5 +11,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int tmp, flag = 0;
 
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			break;
+	}
 }
