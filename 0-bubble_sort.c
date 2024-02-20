7f26e7181aed8a2168a3647e6ac6d5d8808c4603
MazenAtlam <mazenatef5510@gmail.com>
2024-02-20 14:17:05 -0800

    update bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,18 +11,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	 int flag = 0;
+	size_t i, j;
+	int flag = 0, tmp;
 
-	 for(int i = 0; i < size - 1; i++){
-    	 	for(int j = 0; j < size - i - 1; j++){
-         		if(array[j] > array[j + 1]){
-             			int tmp = array[j + 1];
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j + 1];
 				array[j + 1] = array[j];
 				array[j] = tmp;
-             			flag = 1;
-         		}
-    	 	}
-     	if(flag == 0)
-       		break;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			break;
 	}
 }
