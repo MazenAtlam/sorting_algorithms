9780dfcc09da10ad7427659db0bc59c146442517
MarioRaafat <mariorafat10@gmail.com>
2024-02-20 14:02:11 -0800

    Bubble_sort done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,5 +11,18 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	 int flag = 0;
 
+	 for(int i = 0; i < size - 1; i++){
+    	 	for(int j = 0; j < size - i - 1; j++){
+         		if(array[j] > array[j + 1]){
+             			int tmp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = tmp;
+             			flag = 1;
+         		}
+    	 	}
+     	if(flag == 0)
+       		break;
+	}
 }
